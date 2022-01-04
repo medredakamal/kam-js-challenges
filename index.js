9460314b6bb64497d8a7cbe615635565e61a461c
Med Reda Kamal <gencokm@gmail.com>
2022-01-04 01:16:01 +0100

    Challenge 2 : Sorting array numbers in descending order

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ console.log("===== Check if array elements are equal =====");
 console.log(equalElements(["hello", "hello", "hello"]));
 console.log(equalElements(["hello", "hello", "world"]));
 console.log("=== end ===");
+
+// Challenge n°2 : Sorting array numbers in descending order
+const sortArrNumbers = (arr) => {
+  return arr.sort((a, b) => b - a);
+};
+
+// Challenge n°2 Log
+console.log("===== Sorting array numbers in descending order =====");
+console.log(sortArrNumbers([12, 43, 235, 3, 15]));
+console.log("=== end ===");
